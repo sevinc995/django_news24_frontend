e96d969090281b21d3b28b08d5b0a5f7511e784a
sevinc995 <sevincseferova25@gmail.com>
2025-05-25 06:16:19 +0000

    change

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
-    $(document).on("keypress", function (e) {
-        if (e.which == 13) {
-            let url = `https://djangonews24.vercel.app/api/news`;
+
+    $(document).on('keypress', function (e) {
+        if (e.which = 13) {
+            let url = `https://djangonews24.vercel.app/api/news/`;
 
             async function mynews() {
                 let responce = await fetch(url);
@@ -10,17 +11,17 @@ $(document).ready(function () {
                 if (Array.isArray(data.news)) {
                     data.news.forEach(item => {
                         const box = document.createElement('div');
-                        box.className = 'col-md-4'
+                        box.className = 'col-md-4';
                         box.innerHTML = `
                             <div class="box">
                                 <div class="imgbox">
-                                    <img src="${item.img}" alt"">
+                                    <img src="${item.image}" alt="">
                                 </div>
-                                <h1>${item.tit}</h1>
-                                <h4>${item.des}</h4>
+                                <h1>${item.title}</h1>
+                                <h4>${item.description}</h4>
                                 <div class="infobox">
                                     <div class="lbtn">Good</div>
-                                    <div class="lbtn">Bad</div>
+                                    <div class="dbtn">Bad</div>
                                 </div>
                             </div>
                         `;
@@ -28,12 +29,14 @@ $(document).ready(function () {
                     });
                 }
             }
+
             mynews();
         }
     });
+
     $("h2").click(async function (e) {
         e.preventDefault();
-        let url = `https://djangonews24.vercel.app/api/news`;
+        let url = `https://djangonews24.vercel.app/api/news/`;
 
         async function mynews() {
             let responce = await fetch(url);
@@ -45,22 +48,23 @@ $(document).ready(function () {
                     const box = document.createElement('div');
                     box.className = 'col-md-4';
                     box.innerHTML = `
-                        <div class="box">
-                            <div class="imgbox">
-                                <img src="${item.img}" alt"">
+                            <div class="box">
+                                <div class="imgbox">
+                                    <img src="${item.image}" alt="">
+                                </div>
+                                <h1>${item.title}</h1>
+                                <h4>${item.description}</h4>
+                                <div class="infobox">
+                                    <div class="lbtn">Good</div>
+                                    <div class="dbtn">Bad</div>
+                                </div>
                             </div>
-                            <h1>${item.tit}</h1>
-                            <h4>${item.des}</h4>
-                            <div class="infobox">
-                                <div class="lbtn">Good</div>
-                                <div class="lbtn">Bad</div>
-                            </div>
-                        </div>
-                    `;
+                        `;
                     $(".row").append(box);
                 });
             }
         }
+
         mynews();
     });
 });
